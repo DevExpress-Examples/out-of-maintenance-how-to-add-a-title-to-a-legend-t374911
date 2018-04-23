2bd0598e6dd048b3496cd8fcff07702d1c4fa097
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LegendTitleSample/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LegendTitleSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            #region #LegendTitleOptions
+            // Specify legend title's text.
+            chart.Legend.Title.Text = "Area of countries";
+            // Show legend title.
+            chart.Legend.Title.Visible = true;
+            // Configure legend title appearance.
+            chart.Legend.Title.Alignment = StringAlignment.Near;
+            chart.Legend.Title.MaxLineCount = 3;
+            chart.Legend.Title.WordWrap = true;
+            #endregion #LegendTitleOptions
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Add a title to a legend
+
+
+This example demonstrates how to configure a legend title.
+
+
+<h3>Description</h3>
+
+<p>To display the legend title, set the&nbsp;<strong>Legend.Title.Visible</strong>&nbsp;property to true and specify<strong>&nbsp;Legend.Title.Text</strong>. Also, to customize the appearance of the title, it is possible to change, for example, the&nbsp;<strong>Alignment</strong>,&nbsp;<strong>WordWrap</strong>&nbsp;and&nbsp;<strong>MaxLineCount</strong>&nbsp;properties to align the title to left and divide it into several lines.</p>
+
+<br/>
+
+
